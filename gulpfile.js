c2c0b04fb09f460d6669674e9f4fa084530f55f2
wulinpeng <wulinpeng@bytedance.com>
2022-03-26 01:39:24 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var uglify      = require('gulp-uglify');
-var rename      = require('gulp-rename');
-var jade        = require('gulp-jade');
-var babel       = require('gulp-babel');
-var minify      = require('gulp-minify');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var jade = require('gulp-jade');
+var babel = require('gulp-babel');
+var minify = require('gulp-minify');
 var browserSync = require('browser-sync').create();
 
 /**
  * Compile files from _style into css
  */
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     return gulp.src('css/main.scss')
         .pipe(sass({
             includePaths: ['scss'],
@@ -29,7 +29,7 @@ gulp.task('sass', function () {
 /**
  * compile es6
  */
-gulp.task('babel', function () {
+gulp.task('babel', function() {
     gulp.src('src/*.js')
         .pipe(babel({
             presets: ['es2015']
@@ -43,7 +43,7 @@ gulp.task('babel', function () {
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', ['sass', 'babel'], function () {
+gulp.task('watch', ['sass', 'babel'], function() {
     gulp.watch('css/*.scss', ['sass'])
         .on('change', browserSync.reload);
 
